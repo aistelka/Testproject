2891938d45f378d1fc99de61c8318e88191eff47
aistelka <119997883+aistelka@users.noreply.github.com>
2022-12-07 13:34:08 +0530

    Update Testing message

--- 23July 2022/src/testpackage/Basic1st.java
+++ 23July 2022/src/testpackage/Basic1st.java
@@ -3,7 +3,7 @@ package testpackage;
 public class Basic1st {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Velocity");
+		System.out.println("Welcome to Software Testing");
 	}
 
 }
